012ad316dae977874737a49c7991835f51d90308
FikriNoorArafah <fikrinoor790@gmail.com>
2023-03-03 18:58:05 +0700

    login admin

---
+++ admin/resources/views/home/index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Admin</title>
+</head>
+
+<body>
+    <?php if (auth()->check()) : ?>
+        <?php echo auth()->user()->name; ?>
+        <a href="<?php echo e(route('logout.perform')); ?>">logout</a>
+    <?php else : ?>
+        <a href="<?php echo e(route('login.perform')); ?>">login</a>
+    <?php endif; ?>
+
+    <main>
+        <?php if (auth()->check()) : ?>
+            <h1>Dashboard</h1>
+            <p>Only authenticated users can access this section.</p>
+        <?php else : ?>
+            <h1>Homepage</h1>
+            <p>Your viewing the home page. Please login to view the restricted data.</p>
+        <?php endif; ?>
+    </main>
+</body>
+
+</html>
--- admin/routes/web.php
+++ admin/routes/web.php
@@ -11,8 +11,23 @@ use Illuminate\Support\Facades\Route;
 | routes are loaded by the RouteServiceProvider and all of them will
 | be assigned to the "web" middleware group. Make something great!
 |
-*/
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+*/
+
+Route::group(['namespace' => 'App\Http\Controllers'], function()
+{
+    Route::get('/', 'HomeController@index')->name('home.index');
+
+    Route::group(['middleware' => ['guest']], function() {
+        Route::get('/login', 'LoginController@show')->name('login.show');
+        Route::post('/login', 'LoginController@login')->name('login.perform');
+    });
+
+    Route::group(['middleware' => ['auth']], function() {
+        Route::get('/logout', 'LogoutController@perform')->name('logout.perform');
+    });
 });
