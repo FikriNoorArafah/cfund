08590fa336ac51dc77ec41fec7dd4407b4bf2d45
FikriNoorArafah <fikrinoor790@gmail.com>
2023-03-05 21:23:42 +0700

    del admin project

--- database/migrations/2023_03_03_172706_create_companies_table.php
+++ database/migrations/2023_03_03_172706_create_companies_table.php
@@ -12,10 +12,9 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('companies', function (Blueprint $table) {
-            $table->id();
+            $table->id('id_companies');
             $table->string('name');
             $table->string('email')->unique();
-            $table->string('username')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->string('address');
