7ad5ee3268eb1e64dc43fb826d02dc1deaf925a1
FikriNoorArafah <fikrinoor790@gmail.com>
2023-03-14 12:33:31 +0700

    DEPLOY V0.4.2.41

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -14,25 +14,35 @@ class HomeController extends Controller
     public function index()
     {
         $partners = Partner::select('name', 'url_icon')->take(8)->get();
-        $intern = Intern::with([
-            'companies' => function ($query) {
-                return $query->select('company_id', 'name', 'region', 'city', 'url_icon');
-            }, 'majors', 'educations', 'interests', 'levels'
-        ])
+        $user = Auth::user();
+        $intern = Intern::with(['companies', 'majors', 'educations', 'interests', 'levels'])
             ->take(7)
             ->get();
-        $wts = Whattheysay::select('name', 'position', 'comment')->take(3);
 
-        $user = Auth::user();
+        $data = [];
+        foreach ($intern as $program) {
+            $data[] = [
+                'title' => $program->majors->pluck('name')->first(),
+                'url' => $program->companies->url_icon,
+                'company' => $program->companies->name,
+                'region' => $program->companies->region,
+                'city' => $program->companies->city,
+                'kategori' => $program->interests->pluck('name')->first(),
+                'education' => $program->educations->pluck('name'),
+                'level' => $program->levels->pluck('name')->first(),
+            ];
+        }
 
-        return response()([
+        $wts = Whattheysay::select('name', 'position', 'comment')->take(3)->get();
+
+        return response()->json([
             'user' => [
                 'name' => $user->name . ' ' . $user->second_name,
                 'url_icon' => $user->url_icon
             ],
             'partner' => $partners,
             'katamereka' => $wts,
-            'program' => $intern,
+            'program' => $data,
         ]);
     }
 }
