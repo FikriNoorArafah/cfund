9b50ce6c7081dfcfb9426ab303033e001ff336a4
FikriNoorArafah <fikrinoor790@gmail.com>
2023-03-15 22:07:00 +0700

    DEPLOY V0.4.3.2206

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -10,7 +10,7 @@ class UserController extends Controller
     public function index()
     {
         $user = User::all();
-        return response()->json()([
+        return response()->json([
             'user' => $user,
         ]);
     }
